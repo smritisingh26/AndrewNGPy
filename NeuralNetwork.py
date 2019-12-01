fd603b769f84a7bcafb5d6c1341a502e95ba3bfc
smritisingh26 <43320088+smritisingh26@users.noreply.github.com>
2019-12-01 15:28:45 +0530

    Add files via upload
    Week4

---
+++ NeuralNetwork.py
@@ -0,0 +1,137 @@
+#IMPORTING LIBRARIES
+from scipy.io import loadmat
+import numpy as np
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+#LOADING DATA
+data=loadmat('ex3data1.mat')
+#print(data)
+X=data['X']
+y=data['y']
+#print(X.shape, y.shape)
+
+#ONE_HOT_ENCODING
+from sklearn.preprocessing import OneHotEncoder
+encoder = OneHotEncoder(sparse=False)
+y_onehot = encoder.fit_transform(y)
+y_onehot.shape
+
+#DEFINING SIGMOID FUNCTION
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+#DEFINING FORWARD_PROP
+def forward_propagate(X,theta1,theta2):
+    m=X.shape[0]
+    a1 = np.insert(X, 0, values=np.ones(m), axis=1)
+    z2 = a1 * theta1.T
+    a2 = np.insert(sigmoid(z2), 0, values=np.ones(m), axis=1)
+    z3 = a2 * theta2.T
+    h = sigmoid(z3)
+    return a1, z2, a2, z3, h
+
+#DEFINING COST FUNCTION
+def cost(params, input_size, hidden_size, num_labels, X, y, learning_rate):
+    m = X.shape[0]
+    X = np.matrix(X)
+    y = np.matrix(y)
+    # reshaping parameter array into parameter matrices for each layer
+    theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+    theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+    #running forward propogation
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+     # compute the cost
+    J = 0
+    for i in range(m):
+        first_term = np.multiply(-y[i,:], np.log(h[i,:]))
+        second_term = np.multiply((1 - y[i,:]), np.log(1 - h[i,:]))
+        J += np.sum(first_term - second_term)
+
+    J = J / m
+    J += (float(learning_rate) / (2 * m)) * (np.sum(np.power(theta1[:,1:], 2)) + np.sum(np.power(theta2[:,1:], 2)))
+    return J
+
+def sigmoid_gradient(z):
+    return np.multiply(sigmoid(z), (1 - sigmoid(z)))
+
+#BACKPROP
+def backprop(params, input_size, hidden_size, num_labels, X, y, learning_rate):
+    ##### this section is identical to the cost function logic we already saw #####
+    m = X.shape[0]
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    # reshape the parameter array into parameter matrices for each layer
+    theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+    theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+    # run the feed-forward pass
+    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+
+    # initializations
+    J = 0
+    delta1 = np.zeros(theta1.shape)  # (25, 401)
+    delta2 = np.zeros(theta2.shape)  # (10, 26)
+
+    # compute the cost
+    for i in range(m):
+        first_term = np.multiply(-y[i,:], np.log(h[i,:]))
+        second_term = np.multiply((1 - y[i,:]), np.log(1 - h[i,:]))
+        J += np.sum(first_term - second_term)
+
+    J = J / m
+
+    # add the cost regularization term
+    J += (float(learning_rate) / (2 * m)) * (np.sum(np.power(theta1[:,1:], 2)) + np.sum(np.power(theta2[:,1:], 2)))
+
+    ##### end of cost function logic, below is the new part #####
+
+    # perform backpropagation
+    for t in range(m):
+        a1t = a1[t,:]  # (1, 401)
+        z2t = z2[t,:]  # (1, 25)
+        a2t = a2[t,:]  # (1, 26)
+        ht = h[t,:]  # (1, 10)
+        yt = y[t,:]  # (1, 10)
+
+        d3t = ht - yt  # (1, 10)
+
+        z2t = np.insert(z2t, 0, values=np.ones(1))  # (1, 26)
+        d2t = np.multiply((theta2.T * d3t.T).T, sigmoid_gradient(z2t))  # (1, 26)
+
+        delta1 = delta1 + (d2t[:,1:]).T * a1t
+        delta2 = delta2 + d3t.T * a2t
+
+    delta1 = delta1 / m
+    delta2 = delta2 / m
+
+    # add the gradient regularization term
+    delta1[:,1:] = delta1[:,1:] + (theta1[:,1:] * learning_rate) / m
+    delta2[:,1:] = delta2[:,1:] + (theta2[:,1:] * learning_rate) / m
+
+    # unravel the gradient matrices into a single array
+    grad = np.concatenate((np.ravel(delta1), np.ravel(delta2)))
+    return J, grad
+
+J, grad = backprop(params, input_size, hidden_size, num_labels, X, y_onehot, learning_rate)
+J, grad.shape
+
+from scipy.optimize import minimize
+
+# minimize the objective function
+fmin = minimize(fun=backprop, x0=params, args=(input_size, hidden_size, num_labels, X, y_onehot, learning_rate),
+                method='TNC', jac=True, options={'maxiter': 250})
+fmin
+
+X = np.matrix(X)
+theta1 = np.matrix(np.reshape(fmin.x[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
+theta2 = np.matrix(np.reshape(fmin.x[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+
+a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
+y_pred = np.array(np.argmax(h, axis=1) + 1)
+y_pred
+
+correct = [1 if a == b else 0 for (a, b) in zip(y_pred, y)]
+accuracy = (sum(map(int, correct)) / float(len(correct)))
+print ('accuracy = {0}%'.format(accuracy * 100))
