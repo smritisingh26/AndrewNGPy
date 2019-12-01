2a5b624361fdff4586b1cda3aef07621df3d9fff
smritisingh26 <43320088+smritisingh26@users.noreply.github.com>
2019-12-01 15:22:32 +0530

    Add files via upload
    Week2-part2

---
+++ MultivariateLinReg.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('ex1data2.txt', sep = ',', header = None)
+X = data.iloc[:,0:2]
+y = data.iloc[:,2]
+m = len(y)
+#print(data.head())
+X = (X - np.mean(X))/np.std(X)
+ones = np.ones((m,1))
+X = np.hstack((ones, X))
+alpha = 0.01
+num_iters = 400
+theta = np.zeros((3,1))
+y = y[:,np.newaxis]
+
+def computeCostMulti(X, y, theta):
+    temp = np.dot(X, theta) - y
+    return np.sum(np.power(temp, 2)) / (2*m)
+J = computeCostMulti(X, y, theta)
+#print(J)
+
+def gradientDescentMulti(X, y, theta, alpha, iterations):
+    m = len(y)
+    for _ in range(iterations):
+        temp = np.dot(X, theta) - y
+        temp = np.dot(X.T, temp)
+        theta = theta - (alpha/m) * temp
+    return theta
+theta = gradientDescentMulti(X, y, theta, alpha, num_iters)
+print(theta)
+
+J = computeCostMulti(X, y, theta)
+print(J)
