fbbdbc5abd168addbd2ecf610d7b47350690f4f9
smritisingh26 <43320088+smritisingh26@users.noreply.github.com>
2019-12-01 15:26:31 +0530

    Add files via upload
    Week3-Part2

---
+++ RegularizedLogReg.py
@@ -0,0 +1,98 @@
+#IMPORTING LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+#LOADING DATA
+data = pd.read_csv('ex2data2.txt', header = None)
+X = data.iloc[:,:-1]
+y = data.iloc[:,2]
+#print(data.head())
+data.head()
+
+#PLOTTING DATA
+mask = y == 1
+passed = plt.scatter(X[mask][0].values, X[mask][1].values)
+failed = plt.scatter(X[~mask][0].values, X[~mask][1].values,color="red")
+plt.xlabel('Microchip Test1')
+plt.ylabel('Microchip Test2')
+plt.legend((passed, failed), ('Passed', 'Failed'))
+plt.show()
+
+#DEFINING A MAPPING FUNCTION
+def mapFeature(X1,X2):
+    degree=6
+    out = np.ones(X.shape[0])[:,np.newaxis]
+    for i in range (1,degree):
+        for j in range (i+1):
+            out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))[:,np.newaxis]))
+    return out
+X = mapFeature(X.iloc[:,0], X.iloc[:,1])
+
+#DEFINING SIGMOID FUNCTION
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+#DEFINING COST FUNCTION
+def lrCostFunction(theta_t, X_t, y_t, lambda_t):
+    m = len(y_t)
+    J = (-1/m) * (y_t.T @ np.log(sigmoid(X_t @ theta_t)) + (1 - y_t.T) @ np.log(1 - sigmoid(X_t @ theta_t)))
+    reg = (lambda_t/(2*m)) * (theta_t[1:].T @ theta_t[1:])
+    J = J + reg
+    return J
+
+#DEFINING GRADIENT DESCENT
+def lrGradientDescent(theta, X, y, lambda_t):
+    m = len(y)
+    grad = np.zeros([m,1])
+    grad = (1/m) * X.T @ (sigmoid(X @ theta) - y)
+    grad[1:] = grad[1:] + (lambda_t / m) * theta[1:]
+    return grad
+
+#CALCULATING COST FUNCTION
+(m, n) = X.shape
+y = y[:, np.newaxis]
+theta = np.zeros((n,1))
+lmbda = 1
+
+J = lrCostFunction(theta, X, y, lmbda)
+print(J)
+
+#OPTIMISING
+output = opt.fmin_tnc(func = lrCostFunction, x0 = theta.flatten(), fprime = lrGradientDescent, \
+                         args = (X, y.flatten(), lmbda))
+theta = output[0]
+print(theta)
+
+#ACCURACY
+pred = [sigmoid(np.dot(X, theta)) >= 0.5]
+np.mean(pred == y.flatten()) * 100
+
+-----------------------------------------------------------------------------------------------
+#PLOTTING DECISION BOUNDARY
+u = np.linspace(-1, 1.5, 50)
+v = np.linspace(-1, 1.5, 50)
+z = np.zeros((len(u), len(v)))
+
+def mapFeatureForPlotting(X1, X2):
+    degree = 6
+    out = np.ones(1)
+    for i in range(1, degree+1):
+        for j in range(i+1):
+            out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))))
+    return out
+
+for i in range(len(u)):
+    for j in range(len(v)):
+        z[i,j] = np.dot(mapFeatureForPlotting(u[i], v[j]), theta)
+
+mask = y.flatten() == 1
+X = data.iloc[:,:-1]
+passed = plt.scatter(X[mask][0], X[mask][1])
+failed = plt.scatter(X[~mask][0], X[~mask][1])
+plt.contour(u,v,z,0)
+plt.xlabel('Microchip Test1')
+plt.ylabel('Microchip Test2')
+plt.legend((passed, failed), ('Passed', 'Failed'))
+plt.show()
